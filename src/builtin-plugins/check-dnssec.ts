b183bdaeea935e2a296623e2ab1f9b80290a1779
Dániel Földi <dani@stefan.hu>
2022-11-25 10:16:40 +0000

    :construction: Add in progress files

---
+++ src/builtin-plugins/check-dnssec.ts
@@ -0,0 +1,8 @@
+import { CheckPlugin } from 'plugin'
+
+
+export const plugin: CheckPlugin = {
+  name: 'check-dnssec',
+  description: 'Check if the site is using DNSSEC',
+  run: async site => {}
+}
